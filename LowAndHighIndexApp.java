017646f4690734e47f1606202eb8a9983e43ae38
Ap. Stavrou <apostolis.stav@gmail.com>
2022-12-12 21:38:47 +0200

    projects v0.1

---
+++ LowAndHighIndexApp.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.projects;
+
+public class LowAndHighIndexApp {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static int[] getLowAndHighIndexOf(int[] arr, int key) {
+        int low = 0, high = 0;
+        int pivot = 0;
+        boolean found = false;
+
+        if (arr == null) return new int[] {};
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == key) {
+                low = i;
+                found = true;
+                break;
+            }
+        }
+        if (!found) return new int[] {};
+
+        high = low;
+        pivot = low + 1;
+
+        while ((pivot < arr.length) && (arr[pivot++] == key)) {
+            high++;
+        }
+        return new int[] {low,high};
+    }
+}
